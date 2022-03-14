e3b10d93ad2f7ab9fd840977c26bc483ebdc71e1
Rodrigo Porto Schwedersky <rodrigo_schwedersky@estudante.sesisenai.org.br>
2022-03-14 20:44:35 -0300

    esqueleto

---
+++ DEVinBank/Conta.cs
@@ -0,0 +1,26 @@
+﻿namespace DEVinBank
+{
+    public class Conta
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Endereço { get; set; }
+        public decimal RendaMensal { get; set; }
+        public int NumeroConta { get; set; }
+        public int Agencia { get; set; }
+        public decimal Saldo { get; set; }
+
+        public void Saque() { }
+
+        public void Deposito() { }
+
+        public void GetSaldo() { }
+
+        public void Extrato() { }
+
+        public void AlterarDadosCadastrais() { }
+
+        public void Transferencia() { }
+
+    }
+}
